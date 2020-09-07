e51ea723575591a6163e3964e38ef0ecb153c15f
azoozalharte <zezo50115@protonmail.com>
2020-09-07 11:43:00 +0300

    reverse string solution 1

--- reversestring/reversString1.js
+++ reversestring/reversString1.js
@@ -1,9 +1,9 @@
 // Solution 1 For reversing string
 
-function revese(str) {
+function rv(str) {
   // with a revese method
   return str.split("").reverse().join("");
 }
 
-console.log(revese("Azoozka"));
-module.exports = revese;
+console.log(rv("Azoozka"));
+module.exports = rv;
